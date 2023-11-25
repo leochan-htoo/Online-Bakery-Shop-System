b33fecb0b7cc8ac5b28ddf4c5e1b40638de21c7a
leochan-htoo <95844470+leochan-htoo@users.noreply.github.com>
2023-11-26 01:44:13 +0700

    Connect Admin and User part for upate product catagory connection

--- resources/views/home/product.blade.php
+++ resources/views/home/product.blade.php
@@ -7,307 +7,63 @@
        </div>
        <div class="row">
 
+            @foreach ( $product as $product )
 
-          <div class="col-sm-6 col-md-4 col-lg-4">
-             <div class="box">
-                <div class="option_container">
-                   <div class="options">
-                      <a href="" class="option1">
-                      Men's Shirt
-                      </a>
-                      <a href="" class="option2">
-                      Buy Now
-                      </a>
-                   </div>
+                <div class="col-sm-6 col-md-4 col-lg-4">
+                    <div class="box">
+                        <div class="option_container">
+                            <div class="options">
+                                <a href="" class="option1">
+                                Men's Shirt
+                                </a>
+                                <a href="" class="option2">
+                                Buy Now
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="img-box">
+                            <img src="product/{{$product->image}}" alt="">
+                        </div>
+
+                        <div class="detail-box">
+                            <h5>
+                                {{$product->title}}
+                            </h5>
+
+                                @if ($product->discount_price!=null)
+                                        <h6 style="color: red">
+                                            Discount price
+                                            <br>
+                                            {{$product->discount_price}} BTH
+                                        </h6>
+
+
+                                        <h6 style="text-decoration: line-through; color:blue">
+                                            Price
+                                            <br>
+                                            {{$product->price}} BTH
+                                        </h6>
+
+                                    @else
+                                        <h6 style="color:blue">
+                                            Price
+                                            <br>
+                                            {{$product->price}} BTH
+                                        </h6>
+
+
+
+                                @endif
+
+
+
+                        </div>
+                    </div>
                 </div>
-                <div class="img-box">
-                   <img src="images/p1.png" alt="">
-                </div>
-                <div class="detail-box">
-                   <h5>
-                    Sourdough Boule
-                   </h5>
-                   <h6>
-                      $75
-                   </h6>
-                </div>
-             </div>
-          </div>
-          <div class="col-sm-6 col-md-4 col-lg-4">
-             <div class="box">
-                <div class="option_container">
-                   <div class="options">
-                      <a href="" class="option1">
-                      Add To Cart
-                      </a>
-                      <a href="" class="option2">
-                      Buy Now
-                      </a>
-                   </div>
-                </div>
-                <div class="img-box">
-                   <img src="images/p2.png" alt="">
-                </div>
-                <div class="detail-box">
-                   <h5>
-                    Ciabatta
-                   </h5>
-                   <h6>
-                      $80
-                   </h6>
-                </div>
-             </div>
-          </div>
-          <div class="col-sm-6 col-md-4 col-lg-4">
-             <div class="box">
-                <div class="option_container">
-                   <div class="options">
-                      <a href="" class="option1">
-                      Add To Cart
-                      </a>
-                      <a href="" class="option2">
-                      Buy Now
-                      </a>
-                   </div>
-                </div>
-                <div class="img-box">
-                   <img src="images/p3.png" alt="">
-                </div>
-                <div class="detail-box">
-                   <h5>
-                    Baguette
-                   </h5>
-                   <h6>
-                      $68
-                   </h6>
-                </div>
-             </div>
-          </div>
-          <div class="col-sm-6 col-md-4 col-lg-4">
-             <div class="box">
-                <div class="option_container">
-                   <div class="options">
-                      <a href="" class="option1">
-                      Add To Cart
-                      </a>
-                      <a href="" class="option2">
-                      Buy Now
-                      </a>
-                   </div>
-                </div>
-                <div class="img-box">
-                   <img src="images/p4.png" alt="">
-                </div>
-                <div class="detail-box">
-                   <h5>
-                    Rye Loaf
-                   </h5>
-                   <h6>
-                      $70
-                   </h6>
-                </div>
-             </div>
-          </div>
-          <div class="col-sm-6 col-md-4 col-lg-4">
-             <div class="box">
-                <div class="option_container">
-                   <div class="options">
-                      <a href="" class="option1">
-                      Add To Cart
-                      </a>
-                      <a href="" class="option2">
-                      Buy Now
-                      </a>
-                   </div>
-                </div>
-                <div class="img-box">
-                   <img src="images/p5.png" alt="">
-                </div>
-                <div class="detail-box">
-                   <h5>
-                    Whole Wheat Batard
-                   </h5>
-                   <h6>
-                      $75
-                   </h6>
-                </div>
-             </div>
-          </div>
-          <div class="col-sm-6 col-md-4 col-lg-4">
-             <div class="box">
-                <div class="option_container">
-                   <div class="options">
-                      <a href="" class="option1">
-                      Add To Cart
-                      </a>
-                      <a href="" class="option2">
-                      Buy Now
-                      </a>
-                   </div>
-                </div>
-                <div class="img-box">
-                   <img src="images/p13.png" alt="">
-                </div>
-                <div class="detail-box">
-                   <h5>
-                    Focaccia
-                   </h5>
-                   <h6>
-                      $58
-                   </h6>
-                </div>
-             </div>
-          </div>
-          <div class="col-sm-6 col-md-4 col-lg-4">
-             <div class="box">
-                <div class="option_container">
-                   <div class="options">
-                      <a href="" class="option1">
-                      Add To Cart
-                      </a>
-                      <a href="" class="option2">
-                      Buy Now
-                      </a>
-                   </div>
-                </div>
-                <div class="img-box">
-                   <img src="images/p7.png" alt="">
-                </div>
-                <div class="detail-box">
-                   <h5>
-                    Pumpernickel Roll
-                   </h5>
-                   <h6>
-                      $80
-                   </h6>
-                </div>
-             </div>
-          </div>
-          <div class="col-sm-6 col-md-4 col-lg-4">
-             <div class="box">
-                <div class="option_container">
-                   <div class="options">
-                      <a href="" class="option1">
-                      Add To Cart
-                      </a>
-                      <a href="" class="option2">
-                      Buy Now
-                      </a>
-                   </div>
-                </div>
-                <div class="img-box">
-                   <img src="images/p8.png" alt="">
-                </div>
-                <div class="detail-box">
-                   <h5>
-                    Challah Braid
-                   </h5>
-                   <h6>
-                      $65
-                   </h6>
-                </div>
-             </div>
-          </div>
-          <div class="col-sm-6 col-md-4 col-lg-4">
-             <div class="box">
-                <div class="option_container">
-                   <div class="options">
-                      <a href="" class="option1">
-                      Add To Cart
-                      </a>
-                      <a href="" class="option2">
-                      Buy Now
-                      </a>
-                   </div>
-                </div>
-                <div class="img-box">
-                   <img src="images/p9.png" alt="">
-                </div>
-                <div class="detail-box">
-                   <h5>
-                    French Baguette
-                   </h5>
-                   <h6>
-                      $65
-                   </h6>
-                </div>
-             </div>
-          </div>
-          <div class="col-sm-6 col-md-4 col-lg-4">
-             <div class="box">
-                <div class="option_container">
-                   <div class="options">
-                      <a href="" class="option1">
-                      Add To Cart
-                      </a>
-                      <a href="" class="option2">
-                      Buy Now
-                      </a>
-                   </div>
-                </div>
-                <div class="img-box">
-                   <img src="images/p10.png" alt="">
-                </div>
-                <div class="detail-box">
-                   <h5>
-                    Garlic Naan
-                   </h5>
-                   <h6>
-                      $65
-                   </h6>
-                </div>
-             </div>
-          </div>
-          <div class="col-sm-6 col-md-4 col-lg-4">
-             <div class="box">
-                <div class="option_container">
-                   <div class="options">
-                      <a href="" class="option1">
-                      Add To Cart
-                      </a>
-                      <a href="" class="option2">
-                      Buy Now
-                      </a>
-                   </div>
-                </div>
-                <div class="img-box">
-                   <img src="images/p11.png" alt="">
-                </div>
-                <div class="detail-box">
-                   <h5>
-                    Cinnamon Swirl Bread
-                   </h5>
-                   <h6>
-                      $65
-                   </h6>
-                </div>
-             </div>
-          </div>
-          <div class="col-sm-6 col-md-4 col-lg-4">
-             <div class="box">
-                <div class="option_container">
-                   <div class="options">
-                      <a href="" class="option1">
-                      Add To Cart
-                      </a>
-                      <a href="" class="option2">
-                      Buy Now
-                      </a>
-                   </div>
-                </div>
-                <div class="img-box">
-                   <img src="images/p12.png" alt="">
-                </div>
-                <div class="detail-box">
-                   <h5>
-                    Pretzel Bun
-                   </h5>
-                   <h6>
-                      $65
-                   </h6>
-                </div>
-             </div>
-          </div>
+
+            @endforeach
+
        </div>
     </div>
  </section>
